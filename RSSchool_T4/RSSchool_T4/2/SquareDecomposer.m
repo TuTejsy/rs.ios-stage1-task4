4a8c84ea58bf8fe2f0345de99aee9b7829d9957c
TuTejsy <1111kaktus1111@gmail.com>
2020-04-11 17:28:24 +0300

    add solution to task 3

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,37 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+//    NSArray <NSNumber*> *result = @[];
+//    NSArray *uniqueArray = @[];
+//
+//    int step = 0;
+//    int delta = 0;
+//
+//    do {
+//        int intNumber = [number intValue];
+//        int prevVal = intNumber - 1;
+//        result = @[@(prevVal)];
+//
+//        int sum = pow(intNumber, 2);
+//
+//        while(sum > 0) {
+//            sum -= pow(prevVal, 2);
+//            prevVal = sqrt(sum) - delta;
+//
+//            if (prevVal > 0) {
+//                result = [@[@(prevVal)] arrayByAddingObjectsFromArray:result];
+//            }
+//
+//            if (delta > 0) {
+//                delta--;
+//            }
+//        }
+//
+//        uniqueArray = [result valueForKeyPath:@"@distinctUnionOfObjects.self"];
+//
+//    } while ([uniqueArray count] < [result count]);
+//
+//    return result;
+    return nil;
 }
 @end
