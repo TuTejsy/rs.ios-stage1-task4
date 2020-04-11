d9fdb160bb360802e76990a58a4b1a3df0baa304
TuTejsy <1111kaktus1111@gmail.com>
2020-04-12 00:07:02 +0300

    add solution to task2

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,37 +2,69 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-//    NSArray <NSNumber*> *result = @[];
-//    NSArray *uniqueArray = @[];
-//
-//    int step = 0;
-//    int delta = 0;
-//
-//    do {
-//        int intNumber = [number intValue];
-//        int prevVal = intNumber - 1;
-//        result = @[@(prevVal)];
-//
-//        int sum = pow(intNumber, 2);
-//
-//        while(sum > 0) {
-//            sum -= pow(prevVal, 2);
-//            prevVal = sqrt(sum) - delta;
-//
-//            if (prevVal > 0) {
-//                result = [@[@(prevVal)] arrayByAddingObjectsFromArray:result];
-//            }
-//
-//            if (delta > 0) {
-//                delta--;
-//            }
-//        }
-//
-//        uniqueArray = [result valueForKeyPath:@"@distinctUnionOfObjects.self"];
-//
-//    } while ([uniqueArray count] < [result count]);
-//
-//    return result;
-    return nil;
+    NSArray <NSNumber*> *result = @[];
+
+    if ([number intValue] < 5) {
+        return nil;
+    }
+    
+    NSUInteger pos = 0;
+    NSUInteger delta1 = 0;
+    NSUInteger delta2 = 0;
+    NSUInteger delta3 = 0;
+    BOOL isOk = YES;
+
+    do {
+        NSUInteger intNumber = [number intValue];
+        NSUInteger prevVal = intNumber - 1;
+        result = @[@(prevVal)];
+        isOk = YES;
+        NSUInteger sum = pow(intNumber, 2);
+        
+        NSUInteger i = 0;
+        while(sum > 0) {
+            sum -= pow(prevVal, 2);
+            prevVal = sqrt(sum);
+            
+            if (i < pos) {
+                if (i == 0) {
+                    prevVal -= delta1;
+                } else if (i == 1){
+                    prevVal -= delta2;
+                } else {
+                    prevVal -= delta3;
+                }
+            }
+            
+            if (prevVal >= [result[0] intValue]) {
+                isOk = NO;
+                break;
+            }
+
+            if (prevVal > 0) {
+                result = [@[@(prevVal)] arrayByAddingObjectsFromArray:result];
+            }
+            
+            i++;
+        }
+        
+        pos++;
+        if (pos == 5) {
+            pos = 1;
+            if (delta3 > delta2) {
+                delta3 = 0;
+                if (delta2 > delta1) {
+                    delta2 = 0;
+                    delta1++;
+                } else {
+                    delta2++;
+                }
+            } else {
+                delta3++;
+            }
+        }
+    } while (!isOk || [result count] == 1);
+
+    return result;
 }
 @end
--- RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.m
+++ RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.m
@@ -2,7 +2,20 @@
 
 @implementation FullBinaryTrees
 - (NSString *)stringForNodeCount:(NSInteger)count {
-    // good luck
-    return 0;
+    if (count % 2 == 0) {
+        return @"[]";
+    }
+    
+    if (count == 1) {
+        return @"[[0]]";
+    }
+    
+    return @"";
 }
+
++ (NSArray *)getVariantsForNodes: (NSInteger) number ofCount: (NSInteger) count {
+    
+    return @[];
+}
+
 @end
